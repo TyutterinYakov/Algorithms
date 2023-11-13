bc0c1af05fdf7b957ecf7d34fc62e186c2f0009d
YakovTyutterin <tyutterin_yasha@mail.ru>
2023-11-13 15:58:51 +0300

    Str

--- text.c
+++ text.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// //malloc
+// //free
 
 int main() {
-    printf("Hello world!\n");
+
+    int n;
+    scanf("%d", &n);
+
+    int m;
+    scanf("%d", &m);
+
+    int nout[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nout[i]);
+    }
+
+
+    for (int i = 0; i < m; i++) {
+        int min = 0;
+        for (int j = 1; j < n; j++) {
+            if (nout[j] < nout[min]) {
+                min = j;
+            }
+        }
+        printf("%d\n", nout[min]);
+        nout[min]++;
+    }
+
     return 0;
 }
